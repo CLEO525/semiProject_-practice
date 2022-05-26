c710ff8d73898b4f4c3831ba28e6b534e3d61ad9
Jiyeon Kim <cleo0718@gmail.com>
2022-05-26 20:01:26 +0900

    test중입니다

--- src/test/vo/Member.java
+++ src/test/vo/Member.java
@@ -8,7 +8,10 @@ public class Member {
     private String email;
     private int age;
     private Date regdate;
-    public Member() {}
+
+    public Member() {
+    }
+
     public Member(String id, String pwd, String email, int age, Date regdate) {
         super();
         this.id = id;
@@ -17,35 +20,47 @@ public class Member {
         this.age = age;
         this.regdate = regdate;
     }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getPwd() {
         return pwd;
     }
+
     public void setPwd(String pwd) {
         this.pwd = pwd;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public Date getRegdate() {
         return regdate;
     }
+
     public void setRegdate(Date regdate) {
         this.regdate = regdate;
     }
+
+    // testests
 
 }
